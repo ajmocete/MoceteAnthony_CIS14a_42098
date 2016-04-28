da8c03797f56c7e728db4b7dc829b19543bfd244
ajmocete <aj.mocete@gmail.com>
2016-04-27 19:44:20 -0700

    Signed-off-by: ajmocete <aj.mocete@gmail.com>

--- Class/functions.js
+++ Class/functions.js
@@ -52,13 +52,12 @@ function getUrlValues(url){
 	return str;
 }
 
-function setCookie(cname, cvalue, exdays){
-	
-	var d = new Date();
-	d.setTime(d.Time() + (exdays*24*60*60*1000));
-	var expires = "expires="+d.toUTCString();
-	document.cookie = cname + "=" +cvalue + "; " + expires;
-}
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+} 
 
 function getCookie(cname) {
     var name = cname + "=";
