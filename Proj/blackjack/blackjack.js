4e10dc87281fc1fd4aba1e7db3f4514eda7f0c23
ajmocete <aj.mocete@gmail.com>
2016-06-01 19:05:25 -0700

    Signed-off-by: ajmocete <aj.mocete@gmail.com>

--- Proj/blackjack/blackjack.js
+++ Proj/blackjack/blackjack.js
@@ -211,7 +211,7 @@ var controller = {
 		developer.log("controller.playerCount set to: "+controller.playerCount);
 		
 		if(controller.playerCount == 21){
-			controller.DealerTurn();
+			controller.DealerTurn("blackjack");
 			developer.log("player has Blackjack!");
 		}
 		
@@ -240,7 +240,7 @@ var controller = {
 				developer.log("player has bust");
 				view.setButton("Hit", "off");
 			} else if(controller.playerCount == 21){
-				controller.DealerTurn();
+				controller.DealerTurn("blackJack");
 				developer.log("player has Blackjack!");
 			}
 		}
@@ -257,7 +257,12 @@ var controller = {
 				controller.dealerCount = game.CardCount(game.dealersCard, "d");
 				developer.log("DealerCount: "+controller.dealerCount);
 				break;
+			case'blackjack':
+				controller.dealerCount = game.CardCount(game.dealersCard, "d");
+				developer.log("DealerCount: "+controller.dealerCount);
 				
+				
+				break;
 			default:
 				controller.dealerCount = game.CardCount(game.dealersCard, "d");
 				developer.log("DealerCount: "+controller.dealerCount);
